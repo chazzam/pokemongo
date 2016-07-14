3a246aa3b7bc2edf472756bdf17cd7f526ae4610
chazzam <chazz.m@gmail.com>
2016-07-13 23:22:21 -0500

    adding more calculations

--- js/general_calculations.js
+++ js/general_calculations.js
@@ -20,19 +20,23 @@ function getEvolved() {
 }
 
 function getTotal() {
-	var candyPer = (3 + 1);
+	var candyPer = 3;
 	var candy = getCandy();
 	var evolve = getEvolved();
-	var keep = (candy + (2 * ( (candy/evolve) - 1))) / evolve;
-	var scratch = (evolve + 1)/candyPer;
-	var catches = (evolve - candy + 1) / candyPer;
-	if (catches < 0 || candy === evolve || candy === 0) {
+	var keep = (candy + (2 * (candy/evolve) - 1)) / evolve;
+	var keepNoXfer = (candy + candy/evolve - 1) / evolve;
+	var scratch = (evolve + 1)/(candyPer + 1);
+	var scratchNoXfer = evolve/candyPer;
+	var catches = (evolve - candy + 1) / (candyPer + 1);
+	if (catches < 0 || candy === evolve || candy == 0) {
 		catches = 0;
 	}
 	if (keep < 0) {
 		keep = 0;
 	}
 	document.getElementById('keep').innerHTML = Math.floor(keep);
+	document.getElementById('keepNoXfer').innerHTML = Math.floor(keepNoXfer);
 	document.getElementById('scratch').innerHTML = Math.ceil(scratch);
+	document.getElementById('scratchNoXfer').innerHTML = Math.ceil(scratchNoXfer);
 	document.getElementById('catches').innerHTML = Math.ceil(catches);
 }
