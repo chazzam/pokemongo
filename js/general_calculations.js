95411054bbe23e785c6486650bcfe327e165ab6b
chazzam <chazz.m@gmail.com>
2016-07-13 22:45:01 -0500

    Fleshing out the general calculation more

--- js/general_calculations.js
+++ js/general_calculations.js
@@ -1,22 +1,38 @@
 
 
-function getCandy12() {
+function getCandy() {
 	var theForm = document.forms["generalform"];
-	var currentCandy = theForm.elements["currentCandy12"];
+	var currentCandy = theForm.elements["currentCandy"];
 	var howMany = 0;
 	if (currentCandy.value != "") {
 		howMany = parseInt(currentCandy.value);
 	}
 	return howMany;
 }
+function getEvolved() {
+	var theForm = document.forms["generalform"];
+	var evolve = theForm.elements["evolveCost"];
+	var howMany = 0;
+	if (evolve.value != "") {
+		howMany = parseInt(evolve.value);
+	}
+	return howMany;
+}
 
 function getTotal() {
-	var candy = (3 + 1);
-	var scratch12 = (12 + 1)/candy;
-	var current12 = ( (12 - getCandy12()) + 1) / candy;
-	if (current12 < 0) {
-		current12 = 0;
+	var candyPer = (3 + 1);
+	var candy = getCandy();
+	var evolve = getEvolved();
+	var keep = candy + (2 * (candy/evolve - 1)) / evolve;
+	var scratch = (evolve + 1)/candyPer;
+	var current = ( (evolve - candy) + 1) / candyPer;
+	if (current < 0) {
+		current = 0;
 	}
-	document.getElementById('scratch12').innerHTML = scratch12;
-	document.getElementById('current12').innerHTML = current12;
+	if (keep < 0) {
+		keep = 0;
+	}
+	document.getElementById('keep').innerHTML = Math.floor(keep);
+	document.getElementById('scratch').innerHTML = Math.ceil(scratch);
+	document.getElementById('current').innerHTML = Math.ceil(current);
 }
